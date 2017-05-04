368ef74c5f3180785881d2658433c106d3556d16
Nathan Westfall <nathan.westfall@tylertech.com>
2017-05-04 10:12:30 -0400

    initial commit

---
+++ SocketCollectorTest/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace SocketClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Setup Data
+            SocketClient client = new SocketClient();
+
+            Console.WriteLine("Welcome to the Socket Cient Tester");
+            Console.WriteLine();
+            while (!client.VerifyIP())
+            {
+                Console.Write("Enter Socket IP (without port): ");
+                client.IP = Console.ReadLine();
+            }
+            while (client.Port == 0)
+            {
+                Console.Write("Enter Socket Port: ");
+                var portStr = Console.ReadLine();
+                int portNum;
+                if (int.TryParse(portStr, out portNum))
+                    client.Port = portNum;
+            }
+            while (!client.VerifyFile())
+            {
+                Console.Write("Enter path to fake data (xml file only): ");
+                client.FilePath = Console.ReadLine();
+            }
+            Console.Write("How long to wait inbetween sends (in seconds)?: ");
+            var waitStr = Console.ReadLine();
+            int waitNum;
+            if (int.TryParse(waitStr, out waitNum))
+                client.WaitInbetween = waitNum;
+            else
+                client.WaitInbetween = 0;
+            Console.Write("Ready to start? (Y/N) ");
+            var key = Console.ReadLine();
+            if (key.StartsWith("y", StringComparison.CurrentCultureIgnoreCase))
+            {
+                //Start
+                Console.WriteLine("Starting Socket Client!  Stop sending data by clicking any key in the console");
+                bool canConnect = client.Connect();
+                if (!canConnect)
+                    return;
+                client.ReadFile();
+                client.SendData();
+                Console.ReadLine();
+                client.StopSendingData();
+            }
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadLine();
+            return; //Quit
+        }
+    }
+}
---
+++ SocketCollectorTest/SocketClient.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace SocketClient
+{
+    public class SocketClient
+    {
+        public string IP { get; set; }
+        public int Port { get; set; }
+        public string FilePath { get; set; }
+        public int WaitInbetween { get; set; }
+        private string FileData;
+        private IPEndPoint EndPoint;
+        private IPAddress Address;
+        private Socket Socket;
+        private CancellationTokenSource CT;
+
+        public SocketClient() { }
+
+        public bool VerifyIP()
+        {
+            return IPAddress.TryParse(IP, out Address);
+        }
+
+        public bool VerifyFile()
+        {
+            if (string.IsNullOrEmpty(FilePath))
+                return false;
+            return File.Exists(this.FilePath);
+        }
+
+        public void ReadFile()
+        {
+            if(VerifyFile())
+            {
+                FileData = File.ReadAllText(FilePath);
+            }
+        }
+
+        public bool Connect()
+        {
+            if (VerifyIP())
+            {
+                try
+                {
+                    EndPoint = new IPEndPoint(Address, Port);
+
+                    Socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+                    Socket.Connect(EndPoint);
+                    Console.WriteLine("Connected to Socket Collector");
+                    return true;
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine("Error connecting to Socket: " + ex.StackTrace);
+                }
+            }
+            return false;
+        }
+
+        public async void SendData()
+        {
+            CT = new CancellationTokenSource();
+            CT.Token.Register(() =>
+            {
+                Socket.Shutdown(SocketShutdown.Both);
+            });
+            await Task.Run(() =>
+            {
+                byte[] msg = Encoding.ASCII.GetBytes(FileData);
+                while (!CT.IsCancellationRequested)
+                {
+                    try
+                    {
+                        byte[] bytes = new byte[2048];
+                        Console.WriteLine("Sending data to collector...");
+                        int bytesSent = Socket.Send(msg);
+                        Console.WriteLine(string.Format("Sent {0} bytes to collector", bytesSent));
+                    }
+                    catch(Exception ex)
+                    {
+                        Console.WriteLine("Something went wrong while sending data: " + ex.StackTrace);
+                    }
+                    Thread.Sleep(WaitInbetween * 1000);
+                }
+            }, CT.Token);
+        }
+
+        public void StopSendingData()
+        {
+            CT.Cancel();
+        }
+    }
+}
